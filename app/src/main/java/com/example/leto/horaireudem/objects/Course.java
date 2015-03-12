8f8be100281d3835a45716bbb97f1d6f880d2f49
Justin <J1jello@hotmail.com>
2015-03-12 12:41:48 -0400

    Course & Departement

--- app/src/main/java/com/example/leto/horaireudem/objects/Course.java
+++ app/src/main/java/com/example/leto/horaireudem/objects/Course.java
@@ -1,7 +1,65 @@
 package com.example.leto.horaireudem.objects;
 
+import java.util.List;
+
 /**
  * Created by Justin on 2015-03-12.
  */
 public class Course {
+    private Department department;
+    private int courseNumber;
+    private String title;
+    private Session session;
+    private List<CourseSection> courseSection;
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public int getCourseNumber() {
+        return courseNumber;
+    }
+
+    public void setCourseNumber(int courseNumber) {
+        this.courseNumber = courseNumber;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    public void setSession(Session session) {
+        this.session = session;
+    }
+
+    public List<CourseSection> getCourseSection() {
+        return courseSection;
+    }
+
+    public void setCourseSection(List<CourseSection> courseSection) {
+        this.courseSection = courseSection;
+    }
+
+    public String getCourseId(){
+        return getDepartment().getSigle() + getCourseNumber();
+    }
+
+    public Course(){}
+
+    @Override
+    public String toString(){
+        return getCourseId() + " - " + getTitle();
+    }
 }
--- app/src/main/java/com/example/leto/horaireudem/objects/Department.java
+++ app/src/main/java/com/example/leto/horaireudem/objects/Department.java
@@ -1,8 +1,45 @@
 package com.example.leto.horaireudem.objects;
 
+import java.util.List;
+
 /**
  * Created by Justin on 2015-03-12.
  */
 public class Department {
+    private String sigle;
+    private String title;
+    private List<Course> courses;
 
+    public String getSigle() {
+        return sigle;
+    }
+
+    public void setSigle(String sigle) {
+        this.sigle = sigle;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    public Department(){
+
+    }
+
+    @Override
+    public String toString(){
+        return getTitle();
+    }
 }
