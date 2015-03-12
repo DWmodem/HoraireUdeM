22df0c90c2e94b6401d98f029d25a111d2eda916
Justin <J1jello@hotmail.com>
2015-03-12 12:20:21 -0400

    Session

--- app/src/main/java/com/example/leto/horaireudem/objects/Session.java
+++ app/src/main/java/com/example/leto/horaireudem/objects/Session.java
@@ -4,4 +4,27 @@ package com.example.leto.horaireudem.objects;
  * Created by Justin on 2015-03-12.
  */
 public class Session {
+    private SessionSeason season;
+    private int year;
+
+
+    public SessionSeason getSeason() {
+        return season;
+    }
+
+    public void setSeason(SessionSeason season) {
+        this.season = season;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public Session(){
+
+    }
 }
---
+++ app/src/main/java/com/example/leto/horaireudem/objects/SessionSeason.java
@@ -0,0 +1,10 @@
+package com.example.leto.horaireudem.objects;
+
+/**
+ * Created by Justin on 2015-03-12.
+ */
+public enum SessionSeason {
+    Winter,
+    Summer,
+    Autumn
+}
